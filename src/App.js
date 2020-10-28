b697ae7a5c7548f0cb586cac6d262104cdece986
Kunerkhan Akerov <Kunerkhan_Akerov@epam.com>
2020-10-28 20:24:15 +0600

    added example redux-saga

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import PostForm from './components/PostForm';
+import Posts from './components/Posts';
+import FetchedPosts from './components/FetchedPosts';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <PostForm />
+        </div>
+        <div className="row">
+          <div className="col">
+            <h2>Synch</h2>
+            <Posts />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <h2>ASynch</h2>
+            <FetchedPosts />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore, compose, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import  thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { rootReducer } from "./redux/rootReducer";
+import { banWordsMiddleware } from "./redux/middleware";
+import { sagaWatcher } from "./redux/sagas";
+
+const saga = createSagaMiddleware();
+
+const store = createStore(rootReducer, compose(
+    applyMiddleware(thunk, banWordsMiddleware, saga),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+));
+
+saga.run(sagaWatcher);
+
+const app = (
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    app,
   document.getElementById('root')
 );
 
